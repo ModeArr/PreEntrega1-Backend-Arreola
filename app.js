f13a62f5e27926eed99a782b77c7fd1e0cc35cf2
Modesto Rafael Alejandro Arreola Lira <modestorarreola@MacBook-Pro-de-Modesto.local>
2023-12-30 20:59:00 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+class ProductManager {
+    constructor() {
+        this.productos = []
+    }
+
+    getProducts() {
+        return this.productos
+    }
+
+    addProduct(title, description, price, thumbnail, code, stock) {
+/*         if (!(stock>0) || !Number.isNaN(stock)) {
+            console.log('Ingresa un numero mayor a cero')
+            return ''
+        } */
+
+        const producto = {
+            title,
+            description,
+            price,
+            thumbnail,
+            stock,
+            code
+        }
+
+        if (this.productos.lenght === 0) { producto.id = 1 }
+        else { producto.id = this.productos[this.productos.lenght - 1].id + 1 }
+
+        this.productos.push(producto)
+        console.log("Se agrego el producto exitosamente")
+    }
+
+    getProductById(id) {
+        const productIndex = this.productos.findIndex((e) => e.id === id);
+        if (!productIndex) {
+          console.log("Producto no encontrado");
+          return;
+        } else {
+            console.log(this.productos[productIndex])
+        }
+    }
+}
+
+const frutas = new ProductManager()
+frutas.addProduct('Manzana', 'Manzana Red Delicious del Valle', 99, 'manzana.jpg','13235435', 100)
+frutas.addProduct('Pera', 'Pera Williams de temporada', 80, 'pera.jpg', '123456', 80000)
+console.log(frutas.getProducts())
